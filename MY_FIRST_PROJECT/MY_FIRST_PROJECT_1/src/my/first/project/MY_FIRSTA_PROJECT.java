10ec46077dbf92fdefd9d1a7811a70ba10e7ca52
apollyon911 <swampdawg66@gmail.com>
2017-07-18 22:17:57 -0700

    First Project

---
+++ MY_FIRST_PROJECT/MY_FIRST_PROJECT_1/src/my/first/project/MY_FIRSTA_PROJECT.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.first.project;
+
+/**
+ *
+ * @author SWAMPDAWG
+ */
+public class MY_FIRSTA_PROJECT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("My name is: James R Cesena");
+        System.out.println("My favorite color is black");
+        System.out.println("If I won the lottery, I woould hire a body guard for my son and his family,buy a politition, invest in realestate.");
+        
+        
+    }
+    
+}
